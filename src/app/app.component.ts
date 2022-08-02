0fdd38d8d6e1f60138ed4389a93fa9983b12a70e
Leonardo Lima <leo.sopranzetti@hotmail.com>
2022-08-02 14:26:01 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RXJS';
+
+  ngOnInit(): void {
+/*     this.minhaPromise('Eduardo').then(result => console.log(result)); */
+
+/*     this.minhaPromise('José').then(result => console.log(result)).catch(erro => console.log(erro)); */
+
+    this.minhaObservable('Eduardo').subscribe(result => {
+      console.log(result);
+    },
+    erro => {
+      console.log(erro);
+    })
+  }
+
+  minhaPromise(nome: string): Promise<string>{
+    return new Promise((resolve, reject) => {
+      if(nome === 'Eduardo'){
+        setTimeout(() => {
+          resolve('Seja bem vindo Eduardo');
+        }, 1000);
+      } else {
+        reject('Ops! Você não é o Eduardo');
+      }
+    })
+  }
+
+  minhaObservable(nome: string): Observable<string>{
+    return new Observable(subscriber => {
+
+      if(nome === 'Eduardo' ){
+        subscriber.next('Olá! ' + nome);
+        setTimeout(() => {
+          subscriber.next('Resposta com delay');
+        }, 5000);
+      }else {
+        subscriber.error('Ops! Deu erro'); 
+      }
+
+    });
+  }
+
 }
