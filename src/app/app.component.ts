7c80e9cef26582b5054d3b4493b2298b12bd9c6d
Leonardo Lima <leo.sopranzetti@hotmail.com>
2022-08-02 17:01:14 -0300

    Unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,8 +27,15 @@ export class AppComponent implements OnInit {
       complete: () => console.log('cabo')
     }
 
-    const obs = this.minhaObservable('Eduardo');
-    obs.subscribe(observer);
+/*     const obs = this.minhaObservable('Eduardo');
+    obs.subscribe(observer); */
+
+    const obs = this.usuarioObservable('Admin', 'admin@admin');
+    const subs = obs.subscribe(observer);
+
+    setTimeout(() => {
+      subs.unsubscribe();
+    }, 3500);
   }
 
   minhaPromise(nome: string): Promise<string>{
@@ -59,4 +66,47 @@ export class AppComponent implements OnInit {
     });
   }
 
+  usuarioObservable(nome: string, email: string): Observable<Usuario>{
+    return new Observable(subscriber => {
+
+      if(nome === 'Admin'){
+        let usuario = new Usuario(nome, email);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 1000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 2000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 3000);
+
+        setTimeout(() => {
+          subscriber.next(usuario);
+        }, 4000);
+
+        setTimeout(() => {
+          subscriber.complete();
+        }, 5000);
+
+      }else {
+        subscriber.error('Ops! Deu erro'); 
+      }
+
+    });
+  }
+
+}
+
+export class Usuario {
+  constructor(nome: string, email: string){
+    this.nome = nome;
+    this.email = email;
+  }
+
+  nome: string;
+  email: string;
 }
